78775732419a22aeadec81254a48dd10b8d2d006
Maciej Flaga <maciejjjj4@gmail.com>
2026-04-08 20:21:45 +0200

    suicide

--- lab2/main.c
+++ lab2/main.c
@@ -1,11 +1,12 @@
 #include "header.h"
 
 int main(){
+    /* NIESKONCZONA STUDNIA POTENCJALU */
     // parametry
     double L = 5;
-    int N = 500;
+    int N = 1000;
     double dx = L/N;
-    double delta = 1e-15;
+    double delta = 1e-10;
     double m=1.0;
     double hbar=1.0;
     double dtau=0.5*m*dx*dx/hbar;
@@ -17,13 +18,15 @@ int main(){
         V[i] = 0.0;
     }
     double *psi = malloc((N+1)*sizeof(double));
-    double E=met_czas_urojony(L,N,V,delta,psi,x,hbar,m,dx,dtau);
-    double *psi_analityczne = malloc((N+1)*sizeof(double));    
+    double E=met_czas_urojony(L,N,V,delta,psi,x,hbar,m,dx,dtau); // metoda
+    double *psi_analityczne = malloc((N+1)*sizeof(double));
     for (int i=0;i<=N;i++) psi_analityczne[i] = sqrt(2.0/L)*sin(M_PI*x[i]/L);
     FILE *Aa = fopen("Aa.csv","w");
     FILE *Amisc = fopen("Amisc.csv","w");
-    for (int i=0;i<=N;i++) fprintf(Aa,"%lf,%lf,%lf\n",x[i],pow(fabs(psi[i]),2),pow(fabs(psi_analityczne[i]),2));
-    fprintf(Amisc,"%lf,%d,%lf,%e",E,N,L,delta);
+    for (int i=0;i<=N;i++) fprintf(Aa,"%lf,%lf,%lf\n",x[i],pow(fabs(psi[i]),2),pow(fabs(psi_analityczne[i]),2)); // zapis funkcji do pliku
+    fprintf(Amisc,"%lf,%d,%lf,%e",E,N,L,delta); // zapis parametrow do pliku
+    /* SKONCZONA STUDNIA POTENCJALU */
+    
     // czystki
     free(x);
     free(V);
