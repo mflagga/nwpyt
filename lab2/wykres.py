485e288842406eed3f204bc9fcdeea5a5f67810a
Maciej Flaga <maciejjjj4@gmail.com>
2026-04-09 14:00:30 +0200

    changed resolution

--- lab2/wykres.py
+++ lab2/wykres.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 Aa = np.loadtxt("Aa.csv",delimiter=",")
 Amisc = np.loadtxt("Amisc.csv",delimiter=",")
 
-plt.figure(figsize=(12.8,7.2))
+plt.figure(figsize=(9.6,5.4))
 plt.plot(Aa[:,0],Aa[:,1],ls='--',color='black',label=rf'Analityczne; $E_1={np.pi**2/(2*Amisc[2]**2):.5f}$')
 plt.plot(Aa[:,0],Aa[:,2],label=rf'Numeryczne; $E_1={Amisc[0]:.5f}$')
 plt.title(rf'$|E_{{analityczne}}-E_{{numeryczne}}|={abs(np.pi**2/(2*Amisc[2]**2)-Amisc[0]):e}$')
