154901ac96802563f206754b261496a76fc61d73
Maciej Flaga <maciejjjj4@gmail.com>
2026-04-10 18:09:11 +0200

    pierwszy punkt skonczony

--- lab2/main.c
+++ lab2/main.c
@@ -24,9 +24,18 @@ int main(){
     FILE *Aa = fopen("Aa.csv","w");
     FILE *Amisc = fopen("Amisc.csv","w");
     for (int i=0;i<=N;i++) fprintf(Aa,"%lf,%lf,%lf\n",x[i],pow(fabs(psi[i]),2),pow(fabs(psi_analityczne[i]),2)); // zapis funkcji do pliku
-    fprintf(Amisc,"%lf,%d,%lf,%e",E,N,L,delta); // zapis parametrow do pliku
+    // test dla dtau nie spełniającego warunku zbieżności
+    dtau=1.5*m*dx*dx/hbar;
+    double Etest=met_czas_urojony(L,N,V,delta,psi,x,hbar,m,dx,dtau);
+    fprintf(Amisc,"%lf,%d,%lf,%e,%lf",E,N,L,delta,Etest); // zapis parametrow do pliku
+    FILE *Ab=fopen("Ab.csv","w");
+    for (int i=0;i<=N;i++) fprintf(Ab,"%lf\n",pow(fabs(psi[i]),2));
     /* SKONCZONA STUDNIA POTENCJALU */
-    
+    double a=5.0;
+    double xa, xb;
+    for (int L=6; L<=14; L+=2){
+        
+    }
     // czystki
     free(x);
     free(V);
@@ -34,6 +43,7 @@ int main(){
     free(psi_analityczne);
     fclose(Aa);
     fclose(Amisc);
+    fclose(Ab);
     // return zero
     return 0;
 }
