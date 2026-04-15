90e1ca6ab4db0bfb79edac23c6c0015a27ea9eaa
Maciej Flaga <maciejjjj4@gmail.com>
2026-04-16 00:05:32 +0200

    podwaliny

--- lab3/header.cuh
+++ lab3/header.cuh
@@ -5,3 +5,23 @@
 #include <string>
 
 using namespace std;
+
+__global__
+void fill1D(double *x, int n, double d, double xa){
+    int idx = blockIdx.x*blockDim.x+threadIdx.x;
+    if (idx<=n) x[idx]=xa+idx*d;
+}
+
+__global__
+void fillRho2D(double *rho, int nx, int ny, double *x, double *y, double R, double r){
+    int idx = blockIdx.x*blockDim.x+threadIdx.x;
+    int idy = blockIdx.y*blockDim.y+threadIdx.y;
+    if (idx <= nx && idy <=ny){
+        if (hypot(x[idx],y[idy])<R){
+            rho[idx*(ny+1)+idy]=r;
+        }
+        else{
+            rho[idx*(ny+1)+idy]=0.0;
+        }
+    }
+}
--- lab3/main.cu
+++ lab3/main.cu
@@ -1,6 +1,33 @@
 #include "header.cuh"
 
 int main(){
-    cout<<"Hello World!\n";
+    // parametry
+    int nx=128;
+    int ny=128;
+    double L=5.0;
+    double Lx=L;
+    double Ly=L;
+    double dx=Lx/nx;
+    double dy=Ly/ny;
+    int tp1=256;
+    int tp2=8;
+    double R=1.0;
+    double r=10.0;
+    dim3 block2 = dim3(tp2,tp2);
+    dim3 grid2 = dim3((nx+block2.x+1)/block2.x,(ny+block2.y+1)/block2.y);
+    // alokacja
+    double *x; cudaMalloc(&x,(nx+1)*sizeof(double));
+    double *y; cudaMalloc(&y,(ny+1)*sizeof(double));
+    double *rho; cudaMalloc(&rho,(nx+1)*(ny+1)*sizeof(double));
+    // inicjalizacja
+    fill1D<<<(nx+tp1)/tp1,tp1>>>(x,nx,dx,-Lx/2);
+    fill1D<<<(ny+tp1)/tp1,tp1>>>(y,ny,dy,-Ly/2);
+    cudaDeviceSynchronize();
+    fillRho2D<<<grid2,block2>>>(rho,nx,ny,x,y,R,r);
+    // czystki
+    cudaFree(x);
+    cudaFree(y);
+    cudaFree(rho);
+    // return zero
     return 0;
 }
