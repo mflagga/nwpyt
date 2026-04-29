5a98dd65663f6f72141a80ba83269052f6da2099
Maciej Flaga <3flaga@stud30-18>
2026-04-29 10:55:38 +0200

    komit

---
+++ lab5/header.h
@@ -0,0 +1,26 @@
+#define M_PI 3.141592653589793115997963468544185161590576171875
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+
+typedef double complex cmp;
+
+void thomas(cmp *b, cmp *ac, int n, cmp *d, cmp *x){
+    cmp *cprim = calloc(n,sizeof(cmp));
+    cmp *dprim = calloc(n,sizeof(cmp));
+    cprim[0]=ac[0]/b[0];
+    for (int i=1;i<=n-2;i++){
+        cprim[i]=ac[i]/(b[i]-ac[i]*cprim[i-1]);
+    }
+    dprim[0]=d[0]/b[0];
+    for (int i=1;i<n;i++){
+        dprim[i]=(d[i]-ac[i]*dprim[i-1])/(b[i]-ac[i]*cprim[i-1]);
+    }
+    x[n-1]=dprim[n-1];
+    for (int i=n-2;i>=0;i--){
+        x[i]=dprim[i]-cprim[i]*x[i+1];
+    }
+    free(cprim);
+    free(dprim);
+}
---
+++ lab5/main.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+int main(){
+    // parametry jednostkowe
+    double m=1.0;
+    double hbar=1.0;
+
+    // parametry ukladu
+
+    // parametry wtórne
+
+    // czystki
+
+    // return zero
+    return 0;
+}
