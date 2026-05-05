6db613ab574cb32d1f0a923c581063881b8583dc
Maciej <maciejjjj4@gmail.com>
2026-05-05 23:52:14 +0200

    przygotowanie

--- lab6/header.h
+++ lab6/header.h
@@ -1,3 +1,8 @@
+#define M_PI 3.141592653589793115997963468544185161590576171875
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <complex.h>
+#include <time.h>
+
+typedef complex double cmp;
--- lab6/main.c
+++ lab6/main.c
@@ -1,5 +1,15 @@
 #include "header.h"
 
 int main(){
+
+    // parametry jednostkowe
+
+    // parametry układu
+
+    // parametry wtórne
+
+    // czystki
+
+    // return zero
     return 0;
 }
